d230461449b4d7fa578d963bc032097da3ce88e3
teste <arielalves.alves@gmail.com>
2026-02-02 22:19:36 -0400

    JS

---
+++ index.js
@@ -0,0 +1,8 @@
+var leonardo =window.document.getElementById("leonardo");
+var samanata =window.document.getElementById("samanata");
+var bruna =window.document.getElementById("bruna");
+
+function RolarParaDireita(){
+    leonardo.style ="display:none";
+    bruna.style = "display:flex";
+    }
